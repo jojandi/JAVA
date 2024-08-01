f39ad48f09baa3cb68f2ee8cff89dc561b4bf4f5
조민정 <mg518220@gmail.com>
2024-08-01 15:46:25 +0900

    24.08.01 수업

--- chap11/src/sec01/exam08/TimeExam.java
+++ chap11/src/sec01/exam08/TimeExam.java
@@ -1,5 +1,8 @@
 package sec01.exam08;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TimeExam {
 
 	public static void main(String[] args) {
@@ -15,6 +18,17 @@ public class TimeExam {
 		// i가 10억번 도는 데 걸리는 시간
 		// 지금 시간 - time(for문 전에 저장한 시간)
 		System.out.println(System.currentTimeMillis() - time);
+		
+
+		Date date = new Date();
+		System.out.println(date);
+		
+		String format = "yyyy년 MM월 dd일 HH:mm:ss.SSS";
+		SimpleDateFormat sdf = new SimpleDateFormat(format);
+		String strDate = sdf.format(date);
+		System.out.println(strDate);
+		
+		
 	}
 
 }
